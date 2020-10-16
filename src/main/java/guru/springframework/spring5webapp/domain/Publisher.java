d517b877cbee2e798c76b56a34a826efe5944754
Rui Abreu <xptomoar@gmail.com>
2020-10-16 16:59:08 +0100

    Thymeleaf

--- src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -94,7 +94,6 @@ public class Publisher {
                 ", city='" + city + '\'' +
                 ", state='" + state + '\'' +
                 ", zip='" + zip + '\'' +
-                ", books=" + books +
                 '}';
     }
 
